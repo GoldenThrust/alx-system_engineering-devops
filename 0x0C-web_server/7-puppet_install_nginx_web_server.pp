da537002007be1f1ee04726bb06e9d027b374205
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-26 20:12:29 -0700

    Puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,26 +5,30 @@ exec { 'install_nginx':
   unless  => 'dpkg -l | grep -qw nginx',
 }
 
+package { 'nginx':
+  ensure => 'installed',
+}
+
 exec { 'update_package_cache':
   command     => 'apt-get -y update',
   refreshonly => true,
 }
 
 exec { 'allow_nginx_http':
-  command => 'ufw allow Nginx Http',
+  command => 'ufw allow Nginx HTTP',
   unless  => 'ufw status | grep "Nginx Http"',
   require => Exec['install_nginx'],
 }
 
 file { '/var/www/html/index.html':
   ensure  => 'file',
-  content => 'Hello World!',
+  content => 'Hello World!\n',
   require => Exec['install_nginx'],
 }
 
 file { '/var/www/html/404.html':
   ensure  => 'file',
-  content => "Ceci n'est pas une page",
+  content => "Ceci n'est pas une page\n",
   require => Exec['install_nginx'],
 }
 
@@ -50,9 +54,20 @@ server {
         location / {
                 try_files ${uri} ${uri}/ =404;
         }
-}",
+}
+",
   require => Exec['install_nginx'],
   notify  => Service['nginx'],
+}
+
+exec { 'start service':
+  command => 'servive nginx start',
+  require => Exec['install_nginx'],
+}
+
+exec { 'restart service':
+  command => 'servive nginx restart',
+  require => Exec['install_nginx'],
 }
 
 service { 'nginx':
