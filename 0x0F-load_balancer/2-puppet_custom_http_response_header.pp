e0959e2af9912639ae1aa2be9c32a907c4818bcc
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-02 18:24:34 -0700

    Fix Syntax Error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,9 +13,9 @@ exec { 'install_nginx':
 
 exec { 'add_header':
   provider => shell,
-  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By ${HOSTNAME};" /etc/nginx/sites-enabled/default;',
-  require     => Exec['install_nginx'],
-  before      => Exec['restart Nginx'],
+  command  => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
+  require  => Exec['install_nginx'],
+  before   => Exec['restart Nginx'],
 }
 
 exec { 'restart service':
