8013fcdccb15ccf371b011e8ed5417be7a957e09
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-02 17:23:00 -0700

    load balancing

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,26 @@
+# Install Nginx and customize web server header
+
+exec { 'update_package_cache':
+  provider    => shell,
+  command     => 'sudo apt-get -y update',
+  refreshonly => true,
+}
+
+exec { 'install_nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+}
+
+exec { 'add_header':
+  provider => shell,
+  command  => sudo sed -i "/listen 80 default_server;/a add_header X-Served-By ${HOSTNAME};" /etc/nginx/sites-available/default;
+  require     => Exec['install_nginx'],
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart service':
+  provider => shell,
+  command  => 'servive nginx restart',
+  require  => Exec['install_nginx'],
+}
+
